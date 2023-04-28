f16a21c21dbb00652c285df9a741cea201d042bd
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-28 19:48:48 +0600

    Add create, payment_params method to add new payment in payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,4 +6,22 @@ class PaymentsController < ApplicationController
     @categories = @user.categories
     @payment = Payment.new
   end
+
+  def create
+    @user = current_user
+    @payment = @user.payments.build(payment_params)
+
+    if @payment.save
+      redirect_to categories_path, notice: 'Payment was successfully created.'
+    else
+      @categories = @user.categories
+      render :new
+    end
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:name, :amount, category_ids: [])
+  end
 end
