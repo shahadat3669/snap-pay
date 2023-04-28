1cc723a5c8486ad0ec9452270af78dfa25853dd2
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-28 16:39:53 +0600

    Add validations test cases for payment model

---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Payment, type: :model do
+  let(:author) { User.create(name: 'Shahadat Hossain', email: 'shahadat@example.com', password: '12345678') }
+  let(:category) { Category.create(name: 'Category 1', user: author) }
+
+  describe 'validations' do
+    it 'requires a payment name' do
+      payment = Payment.new(amount: 10.0, author:, categories: [category])
+
+      expect(payment).to_not be_valid
+    end
+
+    it 'requires a payment amount' do
+      payment = Payment.new(name: 'Test Payment', author:, categories: [category])
+
+      expect(payment).to_not be_valid
+    end
+
+    it 'requires a author' do
+      payment = Payment.new(name: 'Test Payment', amount: 10.0, categories: [category])
+
+      expect(payment).to_not be_valid
+    end
+
+    it 'is invalid without any categories' do
+      payment = Payment.new(name: 'Test Payment', amount: 10.0, author:)
+
+      expect(payment).to_not be_valid
+    end
+
+    it 'is valid with all required attributes' do
+      payment = Payment.new(name: 'Test Payment', amount: 10.0, author:, categories: [category])
+
+      expect(payment).to be_valid
+    end
+  end
+end
