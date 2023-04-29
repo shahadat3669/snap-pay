d2bf22d1d31917eac0ff56c37afcce1837c509cb
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-30 02:56:37 +0600

    Add seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,8 @@
 # exit on error
 set -o errexit
 
-yarn install
-
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+bundle exec rake db:seed
