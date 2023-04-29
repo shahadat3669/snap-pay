e51f361cda03f3e2ea3f0f70f0de8f1192e6f657
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-30 00:36:57 +0600

    Update routes according to requirement

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'splash/index'
 
-  root 'categories#index'
-  resources :categories
-  resources :payments
+  devise_for :users
+  root 'splash#index'
+
+  get '/user' => "categories#index", :as => :user_root
+
+  resources :categories ,only: [:index, :show, :new, :create] do
+    resources :payments, only: [:new, :create]
+  end
 end
