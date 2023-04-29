a4fdc54ed91437199fdbd3af576953aeb1c79189
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-29 20:59:08 +0600

    Add test cases for categories new views.

---
+++ spec/views/categories/new.html.erb_spec.rb
@@ -0,0 +1,26 @@
+# spec/features/categories_spec.rb
+
+require 'rails_helper'
+
+RSpec.feature 'Categories', type: :feature do
+  let(:user) { User.create(name: 'Shahadat Hossain', email: 'shahadat3669@gmail.com', password: '12345678') }
+
+  describe 'create new category' do
+    before(:each) do
+      sign_in user
+      visit new_category_path
+    end
+
+    it 'displays the correct page title' do
+      expect(page).to have_content('New Category')
+    end
+
+    it 'displays the form Name field' do
+      expect(page).to have_field(placeholder: 'Name')
+    end
+
+    it 'displays the Create category button' do
+      expect(page).to have_button('Create Category')
+    end
+  end
+end
