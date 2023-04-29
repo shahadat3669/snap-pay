df7ab838caaaca8107487e091f460d3111d83647
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-29 17:45:19 +0600

    Add test cases for payments new html file

---
+++ spec/views/payments/new.html.erb_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'payments/new.html.erb', type: :feature do
+  let(:author) { User.create(name: 'Shahadat Hossain', email: 'shahadat@example.com', password: '12345678') }
+  let(:icon) { fixture_file_upload(Rails.root.join('app', 'assets', 'images', 'placeholder.png'), 'image/png') }
+
+  let(:category) { Category.create(name: 'Category 1', icon:, user: author) }
+
+  context 'when logged in' do
+    before(:each) do
+      sign_in author
+      @payment = Payment.create(name: 'Test Payment', amount: 10.00, category_ids: [category.id])
+      visit new_payment_path
+    end
+
+    it 'displays the correct page title' do
+      expect(page).to have_content('New Payment')
+    end
+
+    it 'displays the form Name field' do
+      expect(page).to have_field(placeholder: 'Name')
+    end
+
+    it 'displays the form Amount field' do
+      expect(page).to have_field(placeholder: 'Amount')
+    end
+
+    it 'displays the form categories checkboxes' do
+      expect(page).to have_field("category_#{category.id}")
+    end
+
+    it 'displays the Create Payment button' do
+      expect(page).to have_button('Create Payment')
+    end
+
+    it 'does not create a new payment with invalid input' do
+      fill_in 'Name', with: ''
+      fill_in 'Amount', with: -10.00
+      click_button 'Create Payment'
+      expect(page).to have_content('New Payment')
+    end
+  end
+end
